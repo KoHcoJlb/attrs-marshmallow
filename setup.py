f3387c90b03021556f095520c013b71fa56a00d0
Igor Stuzhuk (KoHcoJlb) <fujitsuigor@gmail.com>
2019-09-16 18:29:11 +0300

    poetry

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="attrs-marshmallow",
-    version="1.0",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    install_requires=["attrs", "marshmallow", "typing_inspect"]
-)
