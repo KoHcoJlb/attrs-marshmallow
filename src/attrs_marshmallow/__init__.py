cce9b0f1845ba6c8feb2e841fd1a028758ae4233
Igor Stuzhuk (KoHcoJlb) <fujitsuigor@gmail.com>
2019-12-21 17:43:08 +0200

    improve fields creation, new metadata dsl

--- src/attrs_marshmallow/__init__.py
+++ src/attrs_marshmallow/__init__.py
@@ -1,16 +1,14 @@
-from functools import partial
-from typing import Type, Callable, Mapping, Any, Optional, MutableMapping
+from typing import Type, Callable, Mapping, Any, Optional, ForwardRef, List, Dict
 
 import attr
 import marshmallow
 from marshmallow import Schema, post_load
-from marshmallow.fields import Field, Raw, Dict, Nested, List
+from marshmallow.fields import Raw, Field, Nested
 from marshmallow.schema import SchemaMeta
 from typing_inspect import get_origin, get_args, is_optional_type
 
-MARSHMALLOW_FIELD = "marshmallow_field"
-MARSHMALLOW_KWARGS = "marshmallow_kwargs"
 ATTRIBUTE = "attrs_attribute"
+MARSHMALLOW_OPTS = "marshmallow_opts"
 
 class MissingType:
     def __bool__(self):
@@ -25,73 +23,86 @@ class NestedField(Nested):
         if not self.unknown:
             if isinstance(self.parent, Schema):
                 schema.unknown = self.parent.unknown
-            elif isinstance(self.parent, List) or isinstance(self.parent, Dict):
+            elif isinstance(self.parent, marshmallow.fields.List) or isinstance(self.parent, marshmallow.fields.Dict):
                 schema.unknown = self.parent.parent.unknown
         return schema
 
 def schema(**fields):
     return type("Schema", (Schema,), fields)
 
-_SIMPLE_TYPES: MutableMapping[Type, Type[Field]] = {
-    str: marshmallow.fields.String
+def get_marshmallow_opt(field: attr.Attribute, key: str, default=None) -> Any:
+    return field.metadata.get(MARSHMALLOW_OPTS, {}).get(key, default)
+
+FIELD_FOR_ATTR = Callable[[type, attr.Attribute, type, Mapping[str, Any]], Field]
+FIELD_FOR_ATTR_HOOK = Callable[[type, attr.Attribute, type, Mapping[str, Any], FIELD_FOR_ATTR], Optional[Field]]
+
+_SIMPLE_TYPES: Dict[Type, Type[Field]] = {
+    str: marshmallow.fields.String,
+    bool: marshmallow.fields.Boolean,
+    int: marshmallow.fields.Int
 }
 
-_FIELD_FOR_TYPE = Callable[[Type, Mapping[str, Any]], Field]
-_TYPE_HOOK = Callable[[Type, Mapping[str, Any], _FIELD_FOR_TYPE], Field]
+_FIELD_FOR_ATTR_HOOKS: List[FIELD_FOR_ATTR_HOOK] = []
 
-def _field_for_type(tp: Type, field_kwargs: Mapping[str, Any], field_for_type: _FIELD_FOR_TYPE) -> Field:
+def _default_field_for_attribute(cls: type, attribute: attr.Attribute, tp: type, field_kwargs: Mapping[str, Any],
+                                 field_for_attr: FIELD_FOR_ATTR) -> Field:
     origin = get_origin(tp)
     args = get_args(tp)
 
-    field_kwargs = {"required": True, **field_kwargs}
-
     if origin == list:
-        return marshmallow.fields.List(field_for_type(args[0], {}), **field_kwargs)
+        field = marshmallow.fields.List(field_for_attr(cls, attribute, args[0], {}), **field_kwargs)
     elif origin == dict:
-        return Dict(keys=field_for_type(args[0], {}), values=field_for_type(args[1], {}), **field_kwargs)
+        field = marshmallow.fields.Dict(keys=field_for_attr(cls, attribute, args[0], {}),
+                                        values=field_for_attr(cls, attribute, args[1], {}),
+                                        **field_kwargs)
     elif is_optional_type(tp):
-        return field_for_type(args[0], {**field_kwargs, "required": False, "missing": None})
+        field = field_for_attr(cls, attribute, args[0], {**field_kwargs, "required": False, "missing": None})
     elif hasattr(tp, "Schema"):
-        return NestedField(tp.Schema, **field_kwargs)
+        field = NestedField(tp.Schema, **field_kwargs)
+    # Self reference
+    elif tp == cls or isinstance(tp, str) and tp == cls.__name__ \
+            or isinstance(tp, ForwardRef) and tp.__forward_arg__ == cls.__name__:
+        field = NestedField("self", **field_kwargs)
     else:
-        return _SIMPLE_TYPES.get(tp, Raw)(**field_kwargs)
+        field = _SIMPLE_TYPES.get(tp, Raw)(**field_kwargs)
 
-def _field_for_attribute(attribute: attr.Attribute, type_hook: Optional[_TYPE_HOOK]) -> Field:
-    field_kwargs = {**attribute.metadata.get(MARSHMALLOW_KWARGS, {}), ATTRIBUTE: attribute}
-    if attribute.default:
-        field_kwargs["required"] = False
+    return get_marshmallow_opt(attribute, "field", field)
 
-    if type_hook:
-        field_for_type = partial(type_hook)
-        field_for_type.keywords["default"] = partial(_field_for_type, field_for_type=field_for_type)
-    else:
-        field_for_type = partial(_field_for_type)
-        field_for_type.keywords["field_for_type"] = field_for_type
+def attrs_schema(cls: Type, field_for_attr_hook: Optional[FIELD_FOR_ATTR_HOOK] = None,
+                 make_object: bool = True):
+    def field_for_attr(cls: type, attribute: attr.Attribute, tp: type, field_kwargs: Mapping[str, Any]):
+        field_kwargs = {"required": attribute.default is attr.NOTHING, **field_kwargs,
+                        **get_marshmallow_opt(attribute, "kwargs", {}), ATTRIBUTE: attribute}
 
-    field = field_for_type(attribute.type, field_kwargs)
-    return attribute.metadata.get(MARSHMALLOW_FIELD, field)
+        for hook in _FIELD_FOR_ATTR_HOOKS + [field_for_attr_hook]:
+            if hook:
+                res = hook(cls, attribute, tp, field_kwargs, field_for_attr)
+                if res:
+                    return res
 
-def attrs_schema(cls: Type, type_hook: Optional[_TYPE_HOOK] = None):
-    fields = {name: _field_for_attribute(attribute, type_hook)
+        return _default_field_for_attribute(cls, attribute, tp, field_kwargs, field_for_attr)
+
+    fields = {name: field_for_attr(cls, attribute, attribute.type, {})
               for name, attribute
               in attr.fields_dict(cls).items()
-              if attribute.init}
+              if attribute.init and not get_marshmallow_opt(attribute, "skip")}
 
     @post_load
     def make_object_func(self, data, **kwargs):
         return cls(**data)
 
-    fields["make_object"] = make_object_func
+    if make_object:
+        fields["make_object"] = make_object_func
     fields.update({name: getattr(cls, name) for l in SchemaMeta.resolve_hooks(cls).values() for name in l})
 
     return type("Schema", (marshmallow.Schema,), fields)
 
-def add_schema(cls: Type = None, type_hook: _TYPE_HOOK = None):
+def add_schema(cls: Type = None, field_for_attr: FIELD_FOR_ATTR_HOOK = _default_field_for_attribute):
     def wrapper(cls: Type):
         if "__attrs_attrs__" not in cls.__dict__:
             cls = attr.s(cls, auto_attribs=True, kw_only=True)
 
-        cls.Schema = attrs_schema(cls, type_hook=type_hook)
+        cls.Schema = attrs_schema(cls, field_for_attr)
         return cls
 
     if cls:
@@ -99,5 +110,28 @@ def add_schema(cls: Type = None, type_hook: _TYPE_HOOK = None):
 
     return wrapper
 
-def register_field(cls: Type, field: Type[Field]):
+def marshmallow_opts(attribute: Optional[attr.Attribute] = None, *,
+                     skip: Optional[bool] = False,
+                     field: Optional[Field] = None,
+                     kwargs: Optional[Mapping] = None,
+                     data_key: Optional[str] = None):
+    attribute = attribute or attr.ib()
+    opts = attribute.metadata.setdefault(MARSHMALLOW_OPTS, {})
+
+    kwargs = {**opts.get("kwargs", {}), **(kwargs or {})}
+    if data_key is not None:
+        kwargs["data_key"] = data_key
+    opts["kwargs"] = kwargs
+
+    if skip is not None:
+        opts["skip"] = skip
+    if field is not None:
+        opts["field"] = field
+
+    return attribute
+
+def register_simple_field(cls: Type, field: Type[Field]):
     _SIMPLE_TYPES[cls] = field
+
+def register_field_hook(field_for_attr_hook: FIELD_FOR_ATTR_HOOK):
+    _FIELD_FOR_ATTR_HOOKS.append(field_for_attr_hook)
